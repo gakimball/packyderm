a15c515a37fb60a44d8353f746c6ac2a74475ef0
Geoff Kimball <geoff@zurb.com>
2016-02-14 21:53:12 -0800

    Add comments to main function

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var async = require('asyncawait/async');
-var await = require('asyncawait/await');
 var glob = require('glob');
 var fs = require('fs');
 var path = require('path');
 var yaml = require('js-yaml');
 
+// Functions that parse an input string based on the extension of the file it came from
 var parsers = {
   '.json': function(file) {
     return JSON.parse(file);
@@ -14,11 +13,19 @@ var parsers = {
   }
 }
 
+/**
+ * Searches for dependent Bower and npm packages for config files matching the given name.
+ * @param {string|string[]} packages - Names of packages to search for config files.
+ * @param {string} configFile - Name of the config file to search for.
+ * @param {function} cb - Callback to run when all found config files are parsed.
+ * @return {object} Object of config files from found projects.
+ */
 module.exports = function(packages, configFile, cb) {
   var globPaths = [];
   var configList = {};
   var parser = parsers[path.extname(configFile)];
 
+  // The input package list can be a string or array of strings
   if (typeof packages === 'string') {
     packages = [packages];
   }
@@ -29,12 +36,14 @@ module.exports = function(packages, configFile, cb) {
   packages.map(function(name) {
     var configText;
 
+    // Try to load the package from node_modules
     try {
       var fileContents = fs.readFileSync(path.join(process.cwd(), 'node_modules', name, configFile));
       configList[name] = parser(fileContents);
       return;
     }
 
+    // Try to load the package from bower_components
     catch (e) {
       try {
         var fileContents = fs.readFileSync(path.join(process.cwd(), 'bower_components', name, configFile));
@@ -42,6 +51,7 @@ module.exports = function(packages, configFile, cb) {
         return;
       }
 
+      // Log to console if no npm or Bower package was found
       catch (e) {
         console.log('Packyderm: couldn\'t find an npm or Bower package with the name ' + name);
         return;
